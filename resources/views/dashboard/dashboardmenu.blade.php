073297074dea97530575ee658eb985ef5bec499b
iQ <estrizqy@gmail.com>
2023-05-23 23:54:19 +0700

    adding dashboard menu

---
+++ resources/views/dashboard/dashboardmenu.blade.php
@@ -0,0 +1,5 @@
+@extends('dashboard.layout.index')
+
+@section('content')
+
+@endsection
---
+++ resources/views/dashboard/layout/index.blade.php
@@ -0,0 +1,6 @@
+@include('dashboard.layout.head')
+@include('dashboard.layout.menu')
+
+@yield('content')
+
+@include('dashboard.layout.footer')
