b93215e658d20b3dd969c143c7159c0ff72d4c35
MNurRizqiSaputra <putra15x@gmail.com>
2023-06-06 01:26:59 +0700

    memperbagus tampilan overview

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -11,121 +11,38 @@
             <h2 class="content-title">Overview</h2>
         </div>
 
-        <div class="col-12 col-md-6 col-lg-4">
+        <div class="col-12 col-md-6 col-lg-12">
             <div class="statistics-card">
 
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="d-flex flex-column justify-content-between align-items-start">
-                        <h5 class="content-desc">DATA</h5>
+                        <h5 class="content-desc">WELCOME</h5>
 
-                        <h3 class="statistics-value">USER</h3>
+                        <h3 class="statistics-value">DASHBOARD MyTA</h3>
+                        <p>MyTA adalah sebuah website yang menyediakan sistem informasi tugas akhir bagi mahasiswa. Dibuat dengan tujuan untuk mempermudah mahasiswa dalam mengelola dan menyelesaikan tugas akhir mereka dengan lebih efisien. </p>
+                        <br>
+                        <p>MyTA hadir untuk memudahkan Anda dalam mengatur dan melacak tugas akhir Anda dengan lebih efisien. Kami berharap dengan menggunakan MyTA, Anda dapat fokus pada isi tugas akhir Anda dan mencapai hasil terbaik. Selamat menggunakan MyTA dan semoga sukses dalam perjalanan tugas akhir Anda!</p>
                     </div>
-
-                    <button class="btn-statistics">
-                        <img src="./assets/img/global/times.svg" alt="">
-                    </button>
                 </div>
-
-
 
             </div>
         </div>
 
-        <div class="col-12 col-md-6 col-lg-4">
+        <div class="col-12 col-md-6 col-lg-12" >
             <div class="statistics-card">
 
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="d-flex flex-column justify-content-between align-items-start">
-                        <h5 class="content-desc">DATA</h5>
+                        <h5 class="content-desc">Our Support</h5>
 
-                        <h3 class="statistics-value">MAHASISWA</h3>
+                        <div style="display: flex; justify-content: center;">
+                            <img src="./assets/img/global/buatfavicon.png" alt="" style="max-width: 100%; height: 150px; margin: 10px; padding: 5px;">
+                            <img src="./assets/img/global/Kampus Merdeka.png" alt="" style="max-width: 100%; height: 150px; margin: 10px; padding: 5px;">
+                            <img src="./assets/img/global/Kemendikbud.png" alt="" style="max-width: 100%; height: 150px; margin: 10px; padding: 5px;">
+                            <img src="./assets/img/global/msib.jpg" alt="" style="max-width: 100%; height: 150px; margin: 10px; padding: 5px;">
+                        </div>
                     </div>
-
-                    <button class="btn-statistics">
-                        <img src="./assets/img/global/times.svg" alt="">
-                    </button>
                 </div>
-
-
-
-            </div>
-        </div>
-
-        <div class="col-12 col-md-6 col-lg-4">
-            <div class="statistics-card">
-
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="d-flex flex-column justify-content-between align-items-start">
-                        <h5 class="content-desc">DATA</h5>
-
-                        <h3 class="statistics-value">DOSEN PEMBIMBING</h3>
-                    </div>
-
-                    <button class="btn-statistics">
-                        <img src="./assets/img/global/times.svg" alt="">
-                    </button>
-                </div>
-
-
-
-            </div>
-        </div>
-
-        <div class="col-12 col-md-6 col-lg-4">
-            <div class="statistics-card">
-
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="d-flex flex-column justify-content-between align-items-start">
-                        <h5 class="content-desc">DATA</h5>
-
-                        <h3 class="statistics-value">DOSEN PEMBIMBING</h3>
-                    </div>
-
-                    <button class="btn-statistics">
-                        <img src="./assets/img/global/times.svg" alt="">
-                    </button>
-                </div>
-
-
-
-            </div>
-        </div>
-
-        <div class="col-12 col-md-6 col-lg-4">
-            <div class="statistics-card">
-
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="d-flex flex-column justify-content-between align-items-start">
-                        <h5 class="content-desc">DATA</h5>
-
-                        <h3 class="statistics-value">MAHASISWA</h3>
-                    </div>
-
-                    <button class="btn-statistics">
-                        <img src="./assets/img/global/times.svg" alt="">
-                    </button>
-                </div>
-
-
-            </div>
-        </div>
-
-        <div class="col-12 col-md-6 col-lg-4">
-            <div class="statistics-card">
-
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="d-flex flex-column justify-content-between align-items-start">
-                        <h5 class="content-desc">DATA</h5>
-
-                        <h3 class="statistics-value">DOSEN PENGUJI</h3>
-                    </div>
-
-                    <button class="btn-statistics">
-                        <img src="./assets/img/global/times.svg" alt="">
-                    </button>
-                </div>
-
-
 
             </div>
         </div>
