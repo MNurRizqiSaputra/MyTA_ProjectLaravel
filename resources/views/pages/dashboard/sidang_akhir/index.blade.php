d57cbd13cef153542aee373b34df00d89a390ad1
ARZSeries <andytrian6@gmail.com>
2023-06-22 08:41:43 +0700

    perubahan

--- resources/views/pages/dashboard/sidang_akhir/index.blade.php
+++ resources/views/pages/dashboard/sidang_akhir/index.blade.php
@@ -43,25 +43,8 @@
                 <td>{{ $sidangAkhir->tugas_akhir->judul }}</td>
                 <td>{{ $sidangAkhir->tempat }}</td>
                 <td>{{ $sidangAkhir->tanggal }}</td>
-<<<<<<< HEAD
                 <td>{{ $sidangAkhir->waktu }}</td>
                 <td><a class="btn btn-warning btn-sm" href="{{ route('sidang-akhir.show', ['sidangAkhir' => $sidangAkhir->id]) }}">Edit</a></td>
-=======
-                <td>{{ $sidangAkhir->waktu_mulai }}</td>
-                <td>{{ $sidangAkhir->waktu_selesai }}</td>
-                <td><a href="{{ route('sidang-akhir.show', ['sidangAkhir' => $sidangAkhir->id]) }}">Edit</a></td>
-            </tr>
-            @else
-            <tr class="bg-danger">
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $sidangAkhir->tugas_akhir->mahasiswa->user->nama }}</td>
-                <td>{{ $sidangAkhir->tugas_akhir->judul }}</td>
-                <td>{{ $sidangAkhir->tempat }}</td>
-                <td>{{ $sidangAkhir->tanggal }}</td>
-                <td>{{ $sidangAkhir->waktu_mulai }}</td>
-                <td>{{ $sidangAkhir->waktu_selesai }}</td>
-                <td><a href="{{ route('sidang-akhir.show', ['sidangAkhir' => $sidangAkhir->id]) }}">Edit</a></td>
->>>>>>> 29343c2fbbb5eececbdf90fccc1dfede9ae6c492
             </tr>
             @endif
             @endforeach
