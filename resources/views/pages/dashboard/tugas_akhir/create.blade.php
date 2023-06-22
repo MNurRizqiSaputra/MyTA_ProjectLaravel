b45e928cbc96fd42c14d692b8ed929b19c9a7155
MNurRizqiSaputra <putra15x@gmail.com>
2023-06-23 01:23:22 +0700

    Rapihin UI  general

--- resources/views/pages/dashboard/tugas_akhir/create.blade.php
+++ resources/views/pages/dashboard/tugas_akhir/create.blade.php
@@ -5,20 +5,18 @@
 @section('content')
 <div class="content">
     <div class="row">
-        <div class="col-12">
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb ms-2 mb-4">
-                    <li class="breadcrumb-item">
-                        <p>Tugas Akhir</p>
-                    </li>
-                    <li class="breadcrumb-item active">Tambah Data</li>
-                </ol>
-            </nav>
+        <div class="col-12 bg-light text-dark border border-light">
+            <h3 style="text-align:center;">TAMBAH TUGAS AKHIR</h3>
         </div>
-        <div class="col-12">
-            <form action="{{ route('tugas-akhir.store') }}" method="POST" enctype="multipart/form-data">
-                @include('pages.dashboard.tugas_akhir.form', ['tombol' => 'Tambah'])
-            </form>
+
+        <div class="card">
+            <div class="card-body">
+                <div class="col-12">
+                    <form action="{{ route('tugas-akhir.store') }}" method="POST" enctype="multipart/form-data">
+                        @include('pages.dashboard.tugas_akhir.form', ['tombol' => 'Tambah'])
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/pages/dashboard/tugas_akhir/show.blade.php
+++ resources/views/pages/dashboard/tugas_akhir/show.blade.php
@@ -5,32 +5,30 @@
 @section('content')
 <div class="content">
     <div class="row">
-        <div class="col-12">
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb ms-2 mb-4">
-                    <li class="breadcrumb-item">
-                        <p>Tugas Akhir</p>
-                    </li>
-                    <li class="breadcrumb-item active">Detail Data</li>
-                </ol>
-            </nav>
+        <div class="col-12 bg-light text-dark border border-light">
+            <h3 style="text-align:center;">DETAIL TUGAS AKHIR</h3>
         </div>
 
-        <div class="col-12">
-            @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
+        <div class="card">
+            <div class="card-body">
+                <div class="col-12">
+                    @if(session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                    @elseif(session('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
+                    </div>
+                    @endif
+                    <form action="{{ route('tugas-akhir.update', ['tugasAkhir' => $tugasAkhir->id]) }}" method="POST" enctype="multipart/form-data">
+                        @method('PUT')
+                        @include('pages.dashboard.tugas_akhir.form', ['tombol' => 'Edit'])
+                    </form>
+                </div>
             </div>
-            @elseif(session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-            @endif
-            <form action="{{ route('tugas-akhir.update', ['tugasAkhir' => $tugasAkhir->id]) }}" method="POST" enctype="multipart/form-data">
-                @method('PUT')
-                @include('pages.dashboard.tugas_akhir.form', ['tombol' => 'Edit'])
-            </form>
         </div>
     </div>
 </div>
+<br>
 @endsection
